d152c050cb79bcc5f6b48b5547e246def9f0f01b
Ekaterina Tregub <tregub.es@phystech.edu>
2020-09-02 22:29:13 +0300

    Fix bugs and format

--- solve_square_eq.c
+++ solve_square_eq.c
@@ -1,79 +1,113 @@
 #include <stdio.h>
 #include <math.h>
 
-int quad_equation(double a, double b, double c, double* x1, double* x2);
-int is_equal(double x, double y);
+#define EPS 0.00005
+
+enum roots {
+    ERROR = -1,
+    ZERO,
+    ONE,
+    TWO
+};
+
+int quad_equation(double a, double b, double c, double* x1, double* x2); //решает квадратное уравнение и возращает количество корней
+int is_equal(double x, double y); //сравнивает на равенство два числа и возращает резкльтат 0 или 1
 
 int main() {
+    int root = -1;      //количество корней
     double a, b, c, x1 = 0, x2 = 0;
     printf("Enter the coefficients of the quadratic equation a, b, c \n");
     printf("separeted by a space, respectively: ");
-    scanf("%lf %lf %lf", &a, &b, &c);
-    quad_equation(a, b, c, &x1, &x2); 
-    
+
+    if (scanf("%lf %lf %lf", &a, &b, &c) == 3) {
+        root = quad_equation(a, b, c, &x1, &x2);
+    }
+    else {root = quad_equation(a, b, c, &x1, &x2);
+        root = ERROR;
+    }
+
+    switch (root) {
+        case ZERO:
+            printf("There are no roots.\n");
+            break;
+        case ONE:
+            printf("There is one root: x = %lg\n", x1);
+            break;
+        case TWO:
+            printf("There are two roots: x1 = %lg, x2 = %lg\n", x1, x2);
+            break;
+        case ERROR:
+            printf("Error\n");
+            break;
+        default:
+            printf("Error\n");
+    }
     return 0;
 }
 
 int quad_equation(double a, double b, double c, double* x1, double* x2) {
-    double dis = 0, sq = 0; 
-    int n = 0;
-    
+    double dis = 0, sq = 0; //значение дискриминанта и корня из него
+    int root = 0;
+
+    if ((x1 == x2) || (x1 == NULL) || (x2 == NULL)) { //проверка указателей
+            return ERROR;
+    }
+
     if (is_equal(a, 0)) {
         if(is_equal(b, 0)) {
-            n = 0;
-            printf("There are no roots.\n");
+            root = 0;               //0  0  ?
         }
         else if (is_equal(c, 0)) {
-            n = 1;
-            printf("There is one root: x = %.2f \n", *x1);   
-        } 
-        else {
-            n = 1;
-            *x1 = -c / b;
-            printf("There is one root: x = %.2f \n", *x1);
+            root = 1;               //0  1  0
         }
-    } 
+        else {
+            root = 1;               //0  1  1
+            *x1 = -c / b;
+        }
+    }
     else {
         if (is_equal(b, 0)) {
             if (is_equal(c, 0)) {
-                n = 2;
-                printf("There are two identical roots: x1 = x2 = %.2f \n", *x1);
+                root = 2;           //1  0  0
             }
             else {
-                n = 2;
+                root = 2;           //1  0  1
                 *x1 = sqrt(-c / a);
                 *x2 = -*x1;
-                printf("There are two roots: x1 = %.2f, x2 = %.2f \n", *x1, *x2);
             }
-        } 
-        else { 
-            n = 2;
+        }
+        else {
             if (is_equal(c, 0)) {
+                root = 2;           //1  1  0
                 *x2 = -b / a;
-                printf("There are two roots: x1 = %.2f, x2 = %.2f \n", *x1, *x2);
             }
             else {
-                dis = b*b - 4.0*a*c;
+                dis = b*b - 4*a*c;
                 if (dis > 0) {
+                    root = 2;       //1  1  1  Дискриминант > 0
                     sq = sqrt(dis);
-                    *x1 = (-b + sq)/(2.0*a);
-                    *x2 = (-b - sq)/(2.0*a);
-                    printf("There are two roots: x1 = %.2f, x2 = %.2f \n", *x1, *x2);
-                } 
+                    *x1 = (-b + sq)/(2*a);
+                    *x2 = (-b - sq)/(2*a);
+                }
                 else if (is_equal(dis, 0)) {
+                    root = 2;       //1  1  1  Дискриминант = 0
                     *x1 = -b / (2*a);
-                    printf("There are two indentical roots: x1 = x2 = %.2f \n", *x1);
+                    *x2 = *x1;
                 }
                 else if (dis < 0) {
-                    n = 0;
-                    printf("There are no roots.");
+                    root = 0;       //1  1  1  Дискриминант < 0
                 }
             }
         }
     }
-    return n;
+    return root;
 }
 
 int is_equal(double x, double y) {
-    return x-y < 0.00005 ;
+    if ((x == x) && (y == y)) {
+        return x - y < EPS;
+    }
+    else {
+        return ERROR;
+    }
 }
